532bff4f0a4d374527ba6bba7b84ebb6abb02192
Fabricio <fabemanrique@hotmail.com>
2024-04-20 18:55:15 -0300

    [BFE-8] - Add power ups bars to the UI
    Created a ComboBar prefab which is responsible to show an slider with the corresponding color and value
    Created a ComboBarConfig which is an ScriptableObject responsible for having the values needed to setup the visual of the combo bar
    Created a ComboBarController which takes a ComboBarConfig and sets the correct values for the slider and combo bar
    
    Resolves #8

--- Assets/Scripts/UI/PlayerHealthBar.cs
+++ Assets/Scripts/UI/PlayerHealthBar.cs
@@ -7,25 +7,24 @@ namespace Unity.FPS.UI
 {
     public class PlayerHealthBar : MonoBehaviour
     {
-        [Tooltip("Image component dispplaying current health")]
+        [Tooltip("Image component displaying current health")]
         public Image HealthFillImage;
 
         Health m_PlayerHealth;
 
         void Start()
         {
-            PlayerCharacterController playerCharacterController =
-                GameObject.FindObjectOfType<PlayerCharacterController>();
-            DebugUtility.HandleErrorIfNullFindObject<PlayerCharacterController, PlayerHealthBar>(
-                playerCharacterController, this);
+            PlayerCharacterController playerCharacterController = GameObject.FindObjectOfType<PlayerCharacterController>();
+            DebugUtility.HandleErrorIfNullFindObject<PlayerCharacterController, PlayerHealthBar>(playerCharacterController, this);
 
             m_PlayerHealth = playerCharacterController.GetComponent<Health>();
-            DebugUtility.HandleErrorIfNullGetComponent<Health, PlayerHealthBar>(m_PlayerHealth, this,
-                playerCharacterController.gameObject);
+            DebugUtility.HandleErrorIfNullGetComponent<Health, PlayerHealthBar>(m_PlayerHealth, this, playerCharacterController.gameObject);
         }
 
         void Update()
         {
+            //TODO: this could be implemented as an event on when the player gets hit to
+            // avoid this calculation on every frame
             // update health bar value
             HealthFillImage.fillAmount = m_PlayerHealth.CurrentHealth / m_PlayerHealth.MaxHealth;
         }
