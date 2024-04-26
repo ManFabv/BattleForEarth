b9e1b4c302dc01b043a1024cc9c0e10ca76556ad
Fabricio <fabemanrique@hotmail.com>
2024-04-26 15:56:23 -0300

    [BFE-113] [BFE-114] - Made the player collider a bit more wider and Fix the screen limits for the crosshair
    We now use the image extents info to calculate screen bounds
    We now have a bigger collider for the player
    
    Resolves #113
    Resolves #114

--- Assets/Scripts/Common/ExtensionMethods/RendererExtensionMethods.cs
+++ Assets/Scripts/Common/ExtensionMethods/RendererExtensionMethods.cs
@@ -1,13 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public static class RendererExtensionMethods
 {
-    public static Vector3 GetRendererBoundsSize(this Renderer renderer)
+    public static Vector3 GetRendererBoundsSizeInWorldUnits(this Renderer renderer, Canvas parentCanvas)
     {
-        if(renderer == null) return Vector3.zero;  //we return zero if the object is null
+        if(renderer == null || parentCanvas == null) return Vector3.zero;  //we return zero if the object is null
         //we calculate the bounds
         Bounds objectToClampRendererBounds = renderer.bounds;
         //we return the size
-        return objectToClampRendererBounds.size;
+        return objectToClampRendererBounds.size / parentCanvas.scaleFactor;
+    }
+
+    public static Vector3 GetImageBoundsSizeInWorldUnits(this Image image)
+    {
+        if (image == null || image.sprite == null) return Vector3.zero;  //we return zero if the object is null
+        //we calculate the bounds
+        Bounds objectToClampRendererBounds = image.sprite.bounds;
+        //we return the size
+        return objectToClampRendererBounds.extents;
+    }
+
+    public static Vector3 GetImageHalfBoundsSizeInWorldUnits(this Image image)
+    {
+        return GetImageBoundsSizeInWorldUnits(image) / 2;
     }
 }
