b09b9f0405ecab5f3b69731feae130c8ed1a208e
Fabricio <fabemanrique@hotmail.com>
2024-05-07 17:48:38 -0300

    Created a single point entry for bootstraping the game objects
    Created a GameBootstrapper which is responsible for handling the game initialization

---
+++ Assets/Scripts/Common/Bootstrapping/GameBootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using Unity.FPS.Game;
+using UnityEngine;
+
+namespace Assets.Scripts.Common.Bootstrapping
+{
+    public static class GameBootstrapper
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Initialize()
+        {
+            //TODO: implement VContainer and MessagePipe initialization
+            //this is called only once per execution of the game so
+            //we don't need any extra logic to avoid more than one initialization
+        }
+    }
+}
