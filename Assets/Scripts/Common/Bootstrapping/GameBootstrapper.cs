499438abf2743da82a271eb785f6dfbf64f36a65
Fabricio <fabemanrique@hotmail.com>
2024-05-08 17:45:56 -0300

    New way of checking if running tests
    We check if the game is running tests using the active scenes that are setup on the EditorSettings
    Created a UnityScenesGameUtilities class to handle this information

--- Assets/Scripts/Common/Bootstrapping/GameBootstrapper.cs
+++ Assets/Scripts/Common/Bootstrapping/GameBootstrapper.cs
@@ -1,4 +1,4 @@
-﻿using Unity.FPS.Game;
+﻿using Assets.Scripts.Common.UnityGameUtilities;
 using UnityEngine;
 
 namespace Assets.Scripts.Common.Bootstrapping
@@ -8,9 +8,26 @@ namespace Assets.Scripts.Common.Bootstrapping
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void Initialize()
         {
+            //we don't need to do any initialization if we are only running tests
+            if(IsRunningTests())
+            {
+                return;
+            }
             //TODO: implement VContainer and MessagePipe initialization
             //this is called only once per execution of the game so
             //we don't need any extra logic to avoid more than one initialization
+        }
+
+        private static bool IsRunningTests()
+        {
+#if UNITY_EDITOR
+            // if we are on editor, we look if the active scene is in build 
+            // in the best case, test scenes should never be included in build
+            // so this is an accurate way of telling if we are running unit tests
+            return !UnityScenesGameUtilities.IsActiveSceneInBuild();
+#else
+            return false; //outside the editor, we want to always initialize the game packages
+#endif
         }
     }
 }
---
+++ Assets/Scripts/Common/UnityGameUtilities/UnityScenesGameUtilities.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Common.UnityGameUtilities
+{
+    public static class UnityScenesGameUtilities
+    {
+        /// <summary>
+        /// We get all the scenes names that are setup on the editor settings and are also enabled
+        /// </summary>
+        /// <returns>list of the scene names that are enabled on build</returns>
+        public static IEnumerable<string> GetAllActiveSceneNamesInBuild()
+        {
+            IEnumerable<string> scenes = EditorBuildSettings.scenes
+                .Where(scene => scene.enabled)
+                .Select(scene => scene.path);
+
+            return scenes ?? new List<string>();
+        }
+
+        /// <summary>
+        /// we count all the scenes that are setup and enabled on the editor settings
+        /// </summary>
+        /// <returns>the number of active scenes that are included on the editor settings</returns>
+        public static int GetAllActiveSceneCountInBuild()
+        {
+            return GetAllActiveSceneNamesInBuild().Count();
+        }
+
+        /// <summary>
+        /// We check if the current scene is setup on the editor settings
+        /// </summary>
+        /// <returns>true if the current scene is setup in build</returns>
+        public static bool IsActiveSceneInBuild()
+        {
+            Scene activeScene = SceneManager.GetActiveScene();
+            int activeSceneIndex = activeScene.buildIndex;
+            int numberOfScenesOnBuild = GetAllActiveSceneCountInBuild();
+
+            return activeSceneIndex >= 0 && activeSceneIndex < numberOfScenesOnBuild;
+        }
+    }
+}
